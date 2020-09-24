8e008798612e13d443679d2145acdf8d13a2e906
MC Naveen <funmemcn@gmail.com>
2020-09-24 19:13:09 +0530

    Fixed Bugs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
-const PORT = 4000;
+var port = process.env.PORT || 4000;
 
 app.use(cors());
 
